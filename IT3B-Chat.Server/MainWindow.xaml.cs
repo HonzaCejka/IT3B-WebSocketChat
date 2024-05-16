7d2256e27281488f88b6be7ed7335aa9fe8c971d
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2024-05-16 13:30:45 +0200

    achjo

--- IT3B-Chat.Server/MainWindow.xaml.cs
+++ IT3B-Chat.Server/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.WebSockets;
 
 namespace IT3B_Chat.Server
 {
@@ -16,9 +20,23 @@ namespace IT3B_Chat.Server
  /// </summary>
     public partial class MainWindow : Window
     {
+        private HttpListener _listener;
          public MainWindow()
          {
             InitializeComponent();
+            
          }
+        private async void StartServer()
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://localhost:8080/");
+            _listener.Start();
+            AddMessage();
+        }
+
+        private void AddMessage()
+        {
+            Dispatcher.Invoke();
+        }
     }
 }
