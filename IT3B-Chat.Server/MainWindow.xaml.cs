39c6c8ec1415262015b532198332e864fc8c1979
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2024-05-16 13:06:35 +0200

    design

--- IT3B-Chat.Server/MainWindow.xaml.cs
+++ IT3B-Chat.Server/MainWindow.xaml.cs
@@ -14,11 +14,11 @@ namespace IT3B_Chat.Server
  /// <summary>
  /// Interaction logic for MainWindow.xaml
  /// </summary>
- public partial class MainWindow : Window
- {
-  public MainWindow()
-  {
-   InitializeComponent();
-  }
- }
+    public partial class MainWindow : Window
+    {
+         public MainWindow()
+         {
+            InitializeComponent();
+         }
+    }
 }
